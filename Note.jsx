db6b0b7b2ef9eb1adc141b3ad5f64f8fc8d89d3d
ImPrat <85438371+ImPrat@users.noreply.github.com>
2021-06-06 16:48:59 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> About Me </h1>
+      <p>
+        I am a 20 year old Enginering student, studying AI and Data Science
+      </p>
+    </div>
+  );
+}
+
+export default Note;
